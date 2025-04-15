49b424270fbb99b6cf7079f18e01c000a26d0c44
Siam003 <ferdoussiam@gmail.com>
2025-04-15 17:51:59 +0600

    create env file and change api url to env

--- frontend/src/services/jobService.js
+++ frontend/src/services/jobService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = 'http://localhost:8010/api'
+const API = import.meta.env.VITE_API_BASE_URL
 
 export const getJobs = async () => {
   return await axios.get(`${API}/jobs`)
